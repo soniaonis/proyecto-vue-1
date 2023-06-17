036e7fedb91d0cc692ba233a4504f75815b50ee5
Andrea103 <andreasierra103@gmail.com>
2023-06-17 16:30:35 +0200

    responsive

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,72 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import LoginButton from './components/LoginButton.vue'
-
+import { RouterLink, RouterView } from "vue-router";
+import LoginButton from "./components/LoginButton.vue";
 </script>
 
 <template>
-  <header>
-    <img alt="logo empresa" class="logo" src="@/assets/img/logosync.svg" width="150" height="100" />
-
+  <body>
+    <header>
+      <img
+        alt="logo empresa"
+        class="logo"
+        src="@/assets/img/logosync.svg"
+        width="150"
+        height="100"
+      />
+  
       <nav>
         <RouterLink to="/description">DESCRIPTION</RouterLink>
         <RouterLink to="/features">FEATURES</RouterLink>
         <RouterLink to="/screens">SCREENS</RouterLink>
         <RouterLink to="/extra">EXTRA</RouterLink>
-        <LoginButton/>
-      </nav> 
-  </header>
+        <LoginButton />
+      </nav>
+    </header>
+  </body>
 
   <RouterView />
 </template>
 
 <style scoped>
-body{
-    font-family: 'Open Sans', sans-serif;
-    background-color: rgb(242, 246, 251) ;
+body {
+  font-family: "Open Sans", sans-serif;
+  background-color: rgb(242, 246, 251);
 }
 
 .logo {
-    width: 90px;
-    height: 40px;
-    padding-left: 70px;
-    margin-top: 30px;
+  width: 190px;
+  height: 40px;
+  padding-left: 70px;
 }
- 
+body {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
 header {
-    position: sticky;
-    top: 0;
+  display: flex;
+  flex-wrap: wrap;
+  position: sticky;
+  top: 0;
+  left: 0;
+  gap: 3vw;
 }
-  
+
 nav {
-    display: flex;
-    justify-content: flex-end;
-    color:black;
-    list-style: none;
-    align-items: center;
-    gap: 30px;
-    font-size: 12px;
-    margin-right: 100px;
-    margin-top: -30px;
-    text-decoration: none;  
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-end;
+  color: black;
+  list-style: none;
+  align-items: center;
+  gap: 30px;
+  font-size: 12px;
+  text-decoration: none;
 }
- 
+
 a {
   color: black;
   text-decoration: none;
   font-weight: bold;
-} 
+}
 </style>
