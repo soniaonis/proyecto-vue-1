3ef14754474ff314e561db978d49ec4a91070316
Andrea103 <andreasierra103@gmail.com>
2023-06-17 15:54:32 +0200

    Formulario de login y logica

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,3 +1,51 @@
+<script setup>
+import { ref } from 'vue';
+// let showPassword = false; equivalente en javascript.
+
+const showPassword = ref(false);
+
+const togglePasswordVisibility = () => {
+  showPassword.value = !showPassword.value
+}
+</script>
+
 <template>
-    <h1>Esto es login view</h1>
+  <section>
+    <h1>welcome</h1>
+    <form>
+      <input type="text" placeholder="user name" />
+      <input
+        :type="showPassword ? 'text' : 'password'"
+        placeholder="password"
+      />
+            <!-- el tipo texto muestra los caracteres y el tipo password esconde los caracteres vasados en el showPassword. -->
+      <button type="button" @click="togglePasswordVisibility">
+        {{ showPassword ? 'Hide' : 'Show' }} Password
+        <!-- if (showPassword) {
+            "hide" } else {
+            "show"} -->
+      </button>
+      <button type="button">Login</button>
+    </form>
+  </section>
 </template>
+
+<style scoped>
+section {
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  gap: 5vh;
+  align-items: center;
+}
+form {
+  display: flex;
+  flex-direction: column;
+  max-width: 400px;
+  gap: 2vh;
+}
+input,
+button {
+  padding: 1vh 2vw;
+}
+</style>
